af1dc924ab2f2ff0a3c21e47f52f295b35f8f42b
Yash Gotecha <yash.gotecha@aimdek.com>
2020-11-26 16:11:17 +0530

    Finished our NY framework

---
+++ NYFramework/NYFramework/NYFramework.h
@@ -0,0 +1,19 @@
+//
+//  NYFramework.h
+//  NYFramework
+//
+//  Created by Yash Gotecha on 26/11/20.
+//  Copyright © 2020 Yash Gotecha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NYFramework.
+FOUNDATION_EXPORT double NYFrameworkVersionNumber;
+
+//! Project version string for NYFramework.
+FOUNDATION_EXPORT const unsigned char NYFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NYFramework/PublicHeader.h>
+
+
---
+++ NYFramework/NYFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  NYFramework
+//
+//  Created by Yash Gotecha on 26/11/20.
+//  Copyright © 2020 Yash Gotecha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class Service {
+    
+    private init() {}
+    
+    public static func showMasssage() -> String {
+        
+        return "hey,this is my first framework"
+    }
+}
